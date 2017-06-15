2c027a8f689b1f465a8dcbde9db8bc380166b2b4
nassim90 <laouiti90@yahoo.fr>
2017-06-15 12:29:13 +0200

    version initiale projet

---
+++ wordpress/wp-content/themes/iconic-one/footer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Footer section template.
+ * @package WordPress
+ * @subpackage Iconic_One
+ * @since Iconic One 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+                <h1 align="center">Avec le soutiens financier de</h1>
+
+		<a ><img src= http://localhost/siteassociation/wordpress/wp-content/uploads/2017/03/logonimes.png alt=""> </a>
+		
+<a><img src= http://localhost/siteassociation/wordpress/wp-content/uploads/2017/03/logogard.png alt=""> </a>
+
+<a><img src= http://localhost/siteassociation/wordpress/wp-content/uploads/2017/03/logoallocfam.png alt=""> </a>
+
+   		<h2 align="center">Nos partenaire</h2>
+
+<a href=http://www.francaslr.fr/francas-gard/accueil-gard.html><img src= http://localhost/siteassociation/wordpress/wp-content/uploads/2017/03/logo-francas.gif alt=""> </a>
+
+<a href=http://www.afd-gard.org/><img src= http://localhost/siteassociation/wordpress/wp-content/uploads/2017/03/logo-afd-gard-30-1.jpg alt=""> </a>
+
+
+<a href=http://www.ecoledesparents.org/><img src= http://localhost/siteassociation/wordpress/wp-content/uploads/2017/03/fnepe_logo.png alt=""> </a>
+
+
+<a href=https://www.maison-europe-nimes.eu/><img src= http://localhost/siteassociation/wordpress/wp-content/uploads/2017/03/ec0361_ee9ba45a85b148f0943219f1e4d54d93.png alt=""> </a>
+
+<a href=https://www.facebook.com/AssociationLesMilleCouleurs/><img src= http://localhost/siteassociation/wordpress/wp-content/uploads/2017/03/5d4937e58b499b4e641ad1756e4bf766-1.png alt=""> </a>
+
+<a href=https://www.facebook.com/Humanimes-Nimes-418502911669673/><img src= http://localhost/siteassociation/wordpress/wp-content/uploads/2017/03/14721649_611173999069229_711515168939224885_n-1.jpg alt=""> </a>
+
+<a href=http://samuelvincent.fr/presentation/><img src= http://localhost/siteassociation/wordpress/wp-content/uploads/2017/03/logo-samuel-vicent1.png alt=""> </a>
+
+
+
+
+
+		<div class="clear"></div>
+		</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+		
+				</div><!-- .site-info -->
+				<div class="clear"></div>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
